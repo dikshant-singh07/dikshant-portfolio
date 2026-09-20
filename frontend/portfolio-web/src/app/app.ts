e623c41c6c10d09599f64f37199c24c16874e63b
Dikshant Kumar Singh <dikshantsingh191022@gmail.com>
2026-09-20 16:31:37 +0530

    add experience section

--- frontend/portfolio-web/src/app/app.ts
+++ frontend/portfolio-web/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
 import { Work } from './components/work/work';
+import { Experience } from './components/experience/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Skills, Work],
+  imports: [RouterOutlet, About, Skills, Work, Experience],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
