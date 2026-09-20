fe52eba06b2fbd4bf260a1df2e7e6b75b7db9241
Dikshant Kumar Singh <dikshantsingh191022@gmail.com>
2026-09-20 15:50:35 +0530

    add work section

--- frontend/portfolio-web/src/app/app.ts
+++ frontend/portfolio-web/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
+import { Work } from './components/work/work';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Skills],
+  imports: [RouterOutlet, About, Skills, Work],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
